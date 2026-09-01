62249768ce5c77cb0215ca86c6a884031d1657fd
x4242564 <amaridb10@gmail.com>
2026-09-01 06:33:23 -0400

    Add unit tests for interpreter functionality

---
+++ src/interpreter_test.py
@@ -0,0 +1,43 @@
+import unittest
+from nodes import *
+from interpreter import Interpreter
+from values import Number
+
+
+class TestInterpreter(unittest.TestCase):
+
+    def test_number_node(self):
+        value = Interpreter().visit(NumberNode(42.42))
+        self.assertEqual(value, Number(42.42))
+
+    def test_individual_operations(self):
+        value = Interpreter().visit(AddNode(NumberNode(4), NumberNode(2)))
+        self.assertEqual(value, Number(6))
+
+        value = Interpreter().visit(SubtractNode(NumberNode(4), NumberNode(2)))
+        self.assertEqual(value, Number(2))
+
+        value = Interpreter().visit(MultiplyNode(NumberNode(4), NumberNode(2)))
+        self.assertEqual(value, Number(8))
+
+        value = Interpreter().visit(PowerNode(NumberNode(4), NumberNode(2)))
+        self.assertEqual(value, Number(16))
+
+        value = Interpreter().visit(DivideNode(NumberNode(4), NumberNode(242)))
+        self.assertAlmostEqual(value.value, 0.01653, 5)
+
+        with self.assertRaises(Exception): # assert that dividing by zero raises an exception
+            Interpreter().visit(DivideNode(NumberNode(42), NumberNode(0)))
+
+    def test_full_expression(self): # tests a full expression with multiple operations
+        # 3 + 4 * (7 - 1) / 3
+        tree = AddNode(
+                NumberNode(3),
+                DivideNode(
+                    MultiplyNode(NumberNode(4), SubtractNode(NumberNode(7), NumberNode(1))),
+                    NumberNode(3)
+                )
+            )
+
+        result = Interpreter().visit(tree)
+        self.assertEqual(result, Number(11))
