e0836283fff8cc5b16292ca04d5a3565eb0fc675
x4242564 <amaridb10@gmail.com>
2026-09-06 17:50:14 -0400

    Refactor logging and debug menu structure in calculator app

--- src/calc.py
+++ src/calc.py
@@ -3,6 +3,7 @@ import argparse
 
 from main import calculate
 import logger
+from logger import log, verbose_log
 
 ap = argparse.ArgumentParser()
 
@@ -10,14 +11,15 @@ ap.add_argument("--debug", "--d", action = "store_true", help="Sets debug mode (
 
 args = ap.parse_args()
 
-logger.log(f"Debug mode: {args.debug}")
+log(f"Debug mode: {args.debug}")
+verbose_log(f"Verbose logging: {logger.verbose}")
 
 
 calculation = ''
 eval = ''
 ans = ''
 
-class MenuFrameWidget(ctk.CTkScrollableFrame):
+class DebugMenu(ctk.CTkScrollableFrame):
     def __init__(self, master, title):
         super().__init__(master, label_text = title)
         self.grid_columnconfigure(0, weight=1)
@@ -25,91 +27,74 @@ class MenuFrameWidget(ctk.CTkScrollableFrame):
         self.btn_dim = ctk.CTkButton(self, text = 'Fetch Window Size', command = master.get_window_size, font = ('Ariel', 14))
         self.btn_dim.grid(row = 0, column = 0, columnspan = 1, padx = 4, pady = 4)
 
-        self.v_log = ctk.CTkCheckBox(self, command = self.toggle_verbose, text="Verbose Logging")
+        self.v_var = ctk.IntVar(value = int(logger.verbose))
+        self.v_log = ctk.CTkCheckBox(self, command = self.toggle_verbose, variable = self.v_var, text="Verbose Logging")
         self.v_log.grid(row = 1, column=0, padx = 4, pady = 4)
 
     def toggle_verbose(self):
-        logger.verbose = ((int(logger.verbose) + 1) % 2) == 1
+        logger.verbose = (self.v_var.get()) == 1
 
-        logger.log(f"Verbose logging: {logger.verbose}")
+        log(f"Verbose logging: {logger.verbose}")
 
-
-        
-class App(ctk.CTk):
-
-    def __init__(self, debug):
-        super().__init__()
-
-        ctk.set_appearance_mode("Light")
-        ctk.set_default_color_theme("blue")
-
-        self.title('DIY Calculator')
-
-        if debug:
-            self.minsize(500,290)
-            self.geometry("500x290")
-        else:
-            self.minsize(265,290)
-            self.geometry("265x290")
+class Calculator(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
 
         self.grid_columnconfigure(list(range(0,4)), weight=1)
+        btn_width = 75
 
         self.result_txt = ctk.CTkTextbox(self, state = "disabled", height = 2, font = ('Arial', 24), wrap = "none")
-        self.result_txt.grid(columnspan = 5)
+        self.result_txt.grid(row = 0, column = 0, columnspan = 5, sticky = "ew", padx = 6, pady = 4)
 
-        self.btn_7 = ctk.CTkButton(self, text = '7', command = lambda: self.add_to_calc(7), font = ('Ariel', 14))
-        self.btn_7.grid(row = 1, column = 0, columnspan = 1, padx = 6, pady = 4)
-        self.btn_8 = ctk.CTkButton(self, text = '8', command = lambda: self.add_to_calc(8), font = ('Ariel', 14))
-        self.btn_8.grid(row = 1, column = 1, columnspan = 1, padx = 6, pady = 4)
-        self.btn_9 = ctk.CTkButton(self, text = '9', command = lambda: self.add_to_calc(9), font = ('Ariel', 14))
-        self.btn_9.grid(row = 1, column = 2, columnspan = 1, padx = 6, pady = 4)
+        self.btn_7 = ctk.CTkButton(self, text = '7', command = lambda: self.add_to_calc(7), font = ('Ariel', 14), width = btn_width)
+        self.btn_7.grid(row = 2, column = 0, sticky = "ew", padx = 6, pady = 4)
+        self.btn_8 = ctk.CTkButton(self, text = '8', command = lambda: self.add_to_calc(8), font = ('Ariel', 14), width = btn_width)
+        self.btn_8.grid(row = 2, column = 1, sticky = "ew", padx = 6, pady = 4)
+        self.btn_9 = ctk.CTkButton(self, text = '9', command = lambda: self.add_to_calc(9), font = ('Ariel', 14), width = btn_width)
+        self.btn_9.grid(row = 2, column = 2, sticky = "ew", padx = 6, pady = 4)
 
-        self.btn_4 = ctk.CTkButton(self, text = '4', command = lambda: self.add_to_calc(4), font = ('Ariel', 14))
-        self.btn_4.grid(row = 2, column = 0, columnspan = 1, padx = 6, pady = 4)
-        self.btn_5 = ctk.CTkButton(self, text = '5', command = lambda: self.add_to_calc(5), font = ('Ariel', 14))
-        self.btn_5.grid(row = 2, column = 1, columnspan = 1, padx = 6, pady = 4)
-        self.btn_6 = ctk.CTkButton(self, text = '6', command = lambda: self.add_to_calc(6), font = ('Ariel', 14))
-        self.btn_6.grid(row = 2, column = 2, columnspan = 1, padx = 6, pady = 4)
+        self.btn_4 = ctk.CTkButton(self, text = '4', command = lambda: self.add_to_calc(4), font = ('Ariel', 14), width = btn_width)
+        self.btn_4.grid(row = 3, column = 0, sticky = "ew", padx = 6, pady = 4)
+        self.btn_5 = ctk.CTkButton(self, text = '5', command = lambda: self.add_to_calc(5), font = ('Ariel', 14), width = btn_width)
+        self.btn_5.grid(row = 3, column = 1, sticky = "ew", padx = 6, pady = 4)
+        self.btn_6 = ctk.CTkButton(self, text = '6', command = lambda: self.add_to_calc(6), font = ('Ariel', 14), width = btn_width)
+        self.btn_6.grid(row = 3, column = 2, sticky = "ew", padx = 6, pady = 4)
 
-        self.btn_1 = ctk.CTkButton(self, text = '1', command = lambda: self.add_to_calc(1), font = ('Ariel', 14))
-        self.btn_1.grid(row = 3, column = 0, columnspan = 1, padx = 6, pady = 4)
-        self.btn_2 = ctk.CTkButton(self, text = '2', command = lambda: self.add_to_calc(2), font = ('Ariel', 14))
-        self.btn_2.grid(row = 3, column = 1, columnspan = 1, padx = 6, pady = 4)
-        self.btn_3 = ctk.CTkButton(self, text = '3', command = lambda: self.add_to_calc(3), font = ('Ariel', 14))
-        self.btn_3.grid(row = 3, column = 2, columnspan = 1, padx = 6, pady = 4)
+        self.btn_1 = ctk.CTkButton(self, text = '1', command = lambda: self.add_to_calc(1), font = ('Ariel', 14), width = btn_width)
+        self.btn_1.grid(row = 4, column = 0, sticky = "ew", padx = 6, pady = 4)
+        self.btn_2 = ctk.CTkButton(self, text = '2', command = lambda: self.add_to_calc(2), font = ('Ariel', 14), width = btn_width)
+        self.btn_2.grid(row = 4, column = 1, sticky = "ew", padx = 6, pady = 4)
+        self.btn_3 = ctk.CTkButton(self, text = '3', command = lambda: self.add_to_calc(3), font = ('Ariel', 14), width = btn_width)
+        self.btn_3.grid(row = 4, column = 2, sticky = "ew", padx = 6, pady = 4)
 
-        self.btn_0 = ctk.CTkButton(self, text = '0', command = lambda: self.add_to_calc(0), font = ('Ariel', 14))
-        self.btn_0.grid(row = 4, column = 1, columnspan = 1, padx = 6, pady = 4)
-        self.btn_lparen = ctk.CTkButton(self, text = '(', command = lambda: self.add_to_calc('('), font = ('Ariel', 14))
-        self.btn_lparen.grid(row = 4, column = 0, columnspan = 1, padx = 6, pady = 4)
-        self.btn_rparen = ctk.CTkButton(self, text = ')', command = lambda: self.add_to_calc(')'), font = ('Ariel', 14))
-        self.btn_rparen.grid(row = 4, column = 2, columnspan = 1, padx = 6, pady = 4)
+        self.btn_0 = ctk.CTkButton(self, text = '0', command = lambda: self.add_to_calc(0), font = ('Ariel', 14), width = btn_width)
+        self.btn_0.grid(row = 5, column = 1, sticky = "ew", padx = 6, pady = 4)
+        self.btn_lparen = ctk.CTkButton(self, text = '(', command = lambda: self.add_to_calc('('), font = ('Ariel', 14), width = btn_width)
+        self.btn_lparen.grid(row = 5, column = 0, sticky = "ew", padx = 6, pady = 4)
+        self.btn_rparen = ctk.CTkButton(self, text = ')', command = lambda: self.add_to_calc(')'), font = ('Ariel', 14), width = btn_width)
+        self.btn_rparen.grid(row = 5, column = 2, sticky = "ew", padx = 6, pady = 4)
 
-        self.btn_plus = ctk.CTkButton(self, text = '+', command = lambda: self.add_to_calc('+'), font = ('Ariel', 14))
-        self.btn_plus.grid(row = 4, column = 3, columnspan = 1, padx = 6, pady = 4)
-        self.btn_minus = ctk.CTkButton(self, text = '-', command = lambda: self.add_to_calc('-'), font = ('Ariel', 14))
-        self.btn_minus.grid(row = 3, column = 3, columnspan = 1, padx = 6, pady = 4)
-        self.btn_multiply = ctk.CTkButton(self, text = '*', command = lambda: self.add_to_calc('*'), font = ('Ariel', 14))
-        self.btn_multiply.grid(row = 2, column = 3, columnspan = 1, padx = 6, pady = 4)
-        self.btn_divide = ctk.CTkButton(self, text = '/', command = lambda: self.add_to_calc('/'), font = ('Ariel', 14))
-        self.btn_divide.grid(row = 1, column = 3, columnspan = 1, padx = 6, pady = 4)
-        self.btn_power = ctk.CTkButton(self, text = '^', command = lambda: self.add_to_calc('^'), font = ('Ariel', 14))
-        self.btn_power.grid(row = 1, column = 3, columnspan = 1, padx = 6, pady = 4)
+        self.btn_plus = ctk.CTkButton(self, text = '+', command = lambda: self.add_to_calc('+'), font = ('Ariel', 14), width = btn_width)
+        self.btn_plus.grid(row = 5, column = 3, sticky = "ew", padx = 6, pady = 4)
+        self.btn_minus = ctk.CTkButton(self, text = '-', command = lambda: self.add_to_calc('-'), font = ('Ariel', 14), width = btn_width)
+        self.btn_minus.grid(row = 4, column = 3, sticky = "ew", padx = 6, pady = 4)
+        self.btn_multiply = ctk.CTkButton(self, text = '*', command = lambda: self.add_to_calc('*'), font = ('Ariel', 14), width = btn_width)
+        self.btn_multiply.grid(row = 3, column = 3, sticky = "ew", padx = 6, pady = 4)
+        self.btn_divide = ctk.CTkButton(self, text = '/', command = lambda: self.add_to_calc('/'), font = ('Ariel', 14), width = btn_width)
+        self.btn_divide.grid(row = 2, column = 3, sticky = "ew", padx = 6, pady = 4)
+        self.btn_power = ctk.CTkButton(self, text = '^', command = lambda: self.add_to_calc('^'), font = ('Ariel', 14), width = btn_width)
+        self.btn_power.grid(row = 1, column = 3, sticky = "ew", padx = 6, pady = 4)
 
-        self.btn_point = ctk.CTkButton(self, text = '.', command = lambda: self.add_to_calc('.'), font = ('Ariel', 14))
-        self.btn_point.grid(row = 5, column = 1, columnspan = 1, padx = 6, pady = 4)
-        self.btn_ans = ctk.CTkButton(self, text = 'Ans', command = lambda: self.add_to_calc(ans), font = ('Ariel', 14))
-        self.btn_ans.grid(row = 5, column = 2, columnspan = 1, padx = 6, pady = 4)
-        self.btn_eval = ctk.CTkButton(self, text = '=', command = self.evaluate_calc, font = ('Ariel', 14))
-        self.btn_eval.grid(row = 5, column = 3, columnspan = 1, padx = 6, pady = 4)
-        self.btn_clear = ctk.CTkButton(self, text = 'C', command = self.clear, font = ('Ariel', 14))
-        self.btn_clear.grid(row = 5, column = 0, columnspan = 1, padx = 6, pady = 4)
-        self.btn_backspace = ctk.CTkButton(self, text = 'Del', command = self.backspace, font = ('Ariel', 14))
-        self.btn_backspace.grid(row = 1, column = 0, columnspan = 1, padx = 6, pady = 4)
-
-        if debug:
-            self.menu_frame = MenuFrameWidget(self, title = "Menu")
-            self.menu_frame.grid(row = 0, rowspan = 6, column=6, padx=10)
+        self.btn_point = ctk.CTkButton(self, text = '.', command = lambda: self.add_to_calc('.'), font = ('Ariel', 14), width = btn_width)
+        self.btn_point.grid(row = 6, column = 1, sticky = "ew", padx = 6, pady = 4)
+        self.btn_ans = ctk.CTkButton(self, text = 'Ans', command = lambda: self.add_to_calc(ans), font = ('Ariel', 14), width = btn_width)
+        self.btn_ans.grid(row = 6, column = 2, sticky = "ew", padx = 6, pady = 4)
+        self.btn_eval = ctk.CTkButton(self, text = '=', command = self.evaluate_calc, font = ('Ariel', 14), width = btn_width)
+        self.btn_eval.grid(row = 6, column = 3, sticky = "ew", padx = 6, pady = 4)
+        self.btn_clear = ctk.CTkButton(self, text = 'C', command = self.clear, font = ('Ariel', 14), width = btn_width)
+        self.btn_clear.grid(row = 6, column = 0, sticky = "ew", padx = 6, pady = 4)
+        self.btn_backspace = ctk.CTkButton(self, text = 'Del', command = self.backspace, font = ('Ariel', 14), width = btn_width)
+        self.btn_backspace.grid(row = 1, column = 0, sticky = "ew", padx = 6, pady = 4)
 
     def delete(self):
         self.result_txt.configure(state = "normal")
@@ -170,12 +155,40 @@ class App(ctk.CTk):
         '''Displays an error message in the result text widget.'''
         self.delete()
         self.insert('Error')
+        
+class App(ctk.CTk):
+
+    def __init__(self, debug):
+        super().__init__()
+
+        ctk.set_appearance_mode("Light")
+        ctk.set_default_color_theme("blue")
+
+        self.title('DIY Calculator')
+
+        if debug:
+            self.minsize(500,290)
+            self.geometry("500x290")
+        else:
+            self.minsize(265,290)
+            self.geometry("265x290")
+
+        self.grid_columnconfigure([0,1], weight = 1)
+
+        self.calculator_frame = Calculator(self)
+        self.calculator_frame.grid(row = 0, column = 0)
+
+        if debug:
+            self.debug_menu_frame = DebugMenu(self, title = "Menu")
+            self.debug_menu_frame.grid(row = 0, column = 1, padx=10)
 
     def get_window_size(self):
         width = self._current_width
         height = self._current_height
 
-        print(f"{width}, {height}")
+        log(f"Window dimensions: {width}, {height}")
+        
 
-app = App(args.debug)
-app.mainloop()
+if __name__ == "__main__":
+    app = App(args.debug)
+    app.mainloop()
